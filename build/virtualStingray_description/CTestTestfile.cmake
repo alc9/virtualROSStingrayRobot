a7c4fcb0c737ed4998517e8c0c4eeee55de4daaa
alc9 <your_github_email>
2022-06-12 14:05:08 +0100

    added robot urdf - working on link dyanmics

---
+++ build/virtualStingray_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alex/projects/stingRay/virtualStingray/src/virtualStingray_description
+# Build directory: /home/alex/projects/stingRay/virtualStingray/build/virtualStingray_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
