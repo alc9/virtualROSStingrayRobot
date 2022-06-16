9992fc101925b5bbc729210986c5a194b88458dc
alc9 <alexanderlewiscunningham@gmail.com>
2022-06-16 11:39:46 +0100

    building read,write hw interfaces adding locks

--- src/stingray_hw_control/src/stingray_hw_interface.cpp
+++ src/stingray_hw_control/src/stingray_hw_interface.cpp
@@ -21,8 +21,20 @@ StingrayHWInterface::~StingrayHWInterface(){
     //delete nh_urdf_;
     delete nh_;
 }
-void StingrayHWInterface::read(ros::Duration &elapsed_time){}
-void StingrayHWInterface::enforceLimits(ros::Duration& period){}
+void StingrayHWInterface::read(ros::Duration &elapsed_time){
+    //goal reached -> what frequency, position and velocity
+    //read registers import subscribed parameters
+    
+    if (((joint_angle_>=joint_angle_goal_*0.99) ^ upwards_) || control_param_lock_){
+        //lock access to control params from subscribers
+        control_param_lock_=true;
+    }
+    return;
+}
+void StingrayHWInterface::enforceLimits(ros::Duration& period){
+    //enforce position and velocity
+    pos_jnt_sat_interface_.enforceLimits(period);
+}
 
 void StingrayHWInterface::write(ros::Duration &elapsed_time){
     //TODO: write position
@@ -30,5 +42,13 @@ void StingrayHWInterface::write(ros::Duration &elapsed_time){
     //for (auto i : waveGenerator){
     //    std::cout<<i<<'\n'; 
     //    }
+    enforceLimits(elapsed_time);
+    if (!control_param_lock_){
+        return;
+    }
+    control_param_lock_=false;
+    //if goal position reached then call waveGenerator
+    //set joint positions and wave - on different threads then wait
+
 }
 
--- src/stingray_hw_control/src/stingray_hw_interface.h
+++ src/stingray_hw_control/src/stingray_hw_interface.h
@@ -10,24 +10,41 @@
 #include <array>
 #include <std_msgs/Float32.h>
 
+//Control related functionality: control system eqn, joint motion control and fin wave joint generation
+//@param nh - node handle 
+//@param urdf_model - urdf model for robot
 class StingrayHWInterface : public ros_control_boilerplate::GenericHWInterface{
         StingrayHWInterface(const StingrayHWInterface& s) = delete;
         auto operator=(const StingrayHWInterface& s)->StingrayHWInterface& = delete;
+    protected:
+        ros::NodeHandle* nh_=nullptr;
+        ros::Subscriber* joint_pos_sub_=nullptr;
+        ros::Subscriber* 
     public:
         //manage load urdf model via init
         StingrayHWInterface(ros::NodeHandle &nh,urdf::Model* urdf_model);
+        
         virtual ~StingrayHWInterface();
-        //overwrite base class members
+        
+        //Check if goal position is reached, if true then notify write
         virtual void read(ros::Duration &elapsed_time);
 
         virtual void write(ros::Duration &elapsed_time);
 
         virtual void enforceLimits(ros::Duration &period); 
     private:
-        //set actuator positions
-        //pointer to publisher?
+        void initializeSubscribers(void) noexcept;
+        void init(void) noexcept; 
+        //TODO: pointers to publishers?
         std::array<ros::Publisher,2> actuator_pubs_;
         //offset for actuator positions - left_fin + index
-        ros::NodeHandle* nh_=nullptr;
-        std_msgs::Float32 joint_angle_;
+        std_msgs::Float32 joint_angle_msg_;
+        //
+        float joint_angle_goal_;
+        //current goal joint angle
+        float joint_angle_;
+        //joints are increasing to goal angle
+        bool upwards_;
+        //limit write to control_param_lock_ for subscribers
+        bool control_param_lock_;
 };
